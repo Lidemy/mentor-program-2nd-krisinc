d4f67616bb892e46416bc36ba94abd406c582648
krisinc <ksbeanch@gmail.com>
2018-09-09 13:45:39 +0800

    hw1 complete

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,9 @@
 function reverse(str) {
-
+    var answer = ''
+    for (let i=str.length - 1; i>=0; i--) {
+        answer += str[i]
+    }
+    return answer
 }
+
+console.log(reverse("yoyoyo"))
--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,19 @@
 function join(str, concatStr) {
-
+    let answer = ''
+    for( let i=0; i<str.length; i++) {
+        if(i == str.length - 1) {
+            answer += str[i]
+        }else {
+            answer += str[i] + concatStr
+        }
+    }
+    return answer
 }
 
 function repeat(str, times) {
-
+    let answer = ''
+    for (let i=1; i<=times; i++){
+        answer += str
+    }
+    return answer
 }
